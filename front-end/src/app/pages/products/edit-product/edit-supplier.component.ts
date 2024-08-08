bea711ccd40b491f0a4c8fa5bdfb6070f536c49c
Bladimir Galvez <bladimirgalvez11@mesoamericana.edu.gt>
2024-08-08 14:30:22 -0600

    front de product agregado

---
+++ front-end/src/app/pages/products/edit-product/edit-supplier.component.ts
@@ -0,0 +1,176 @@
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SupplierService } from '../product.service';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+
+
+declare var KTWizard: any; // Declara la variable para evitar errores de TypeScript
+declare var KTUtil: any; // Declara la variable para evitar errores de TypeScript
+
+@Component({
+  selector: 'app-edit-supplier', 
+  standalone: true,
+  templateUrl: './edit-supplier.component.html',
+  styleUrls: ['./edit-supplier.component.css'],
+  imports: [ReactiveFormsModule, CommonModule]
+})
+export class EditSupplierComponent implements OnInit {
+  supplierForm: FormGroup;
+  supplier: any;
+  supplierId: number;
+  private _wizardObj: any;
+  private _formEl: any;
+
+  ngAfterViewInit(): void {
+    this.initWizard();
+  }
+
+  ngOnInit(): void {  
+    if (this.supplier) {
+      this.supplierForm.patchValue({
+        name: this.supplier.name,
+        contact_name: this.supplier.contact_name,
+        contact_email: this.supplier.contact_email,
+        contact_phone: this.supplier.contact_phone,
+        address: this.supplier.address
+      });
+    }
+  }
+
+  private initWizard() {
+    this._wizardObj = new KTWizard('kt_wizard', {
+      startStep: 1,
+      clickableSteps: false
+    });
+    this._formEl = KTUtil.getById('kt_form');
+    this._wizardObj.on('change', (wizard: any) => {
+      if (wizard.getStep() > wizard.getNewStep()) {
+        return; // Skip if stepped back
+      }
+      const step = wizard.getStep();
+
+      // Validar el formulario en cada paso
+      if (step === 1) {
+        const address = this.supplierForm.value.address;
+        const name = this.supplierForm.value.name;
+        const hasNumber = /\d/.test(name);
+        if (!name || hasNumber) {
+          wizard.stop(); // Detener la navegación
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese un nombre válido'
+          });
+          return;
+        }
+        if (!address) {
+          wizard.stop(); // Detener la navegación
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese una dirección válida'
+          });
+          return;
+        }
+      }
+      if (step === 2) {
+        const contact_phone = this.supplierForm.value.contact_phone;
+        const contact_name = this.supplierForm.value.contact_name;
+        const hasletter = /[a-zA-Z]/.test(contact_phone);
+        const contact_email = this.supplierForm.value.contact_email;
+        const hasNumbercontactname = /\d/.test(contact_name);
+        const correctEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(contact_email);
+        if (!contact_name || hasNumbercontactname) {
+          wizard.stop(); // Detener la navegación
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese un nombre de contacto válido'
+          });
+          return;
+        }
+        if (!contact_email || !correctEmail) {
+          wizard.stop(); // Detener la navegación
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese un correo de contacto válido'
+          });
+          return;
+        }
+        if (!contact_phone || hasletter) {
+          wizard.stop(); // Detener la navegación
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese un teléfono de contacto válido'
+          });
+          return;
+        }
+        
+      }
+    });
+
+    this._wizardObj.on('submit', (wizard: any) => {
+      console.log(this.supplierForm.value);
+      if (this.supplierForm.valid) {
+        this.supplierService.updateSupplier(this.supplierId,this.supplierForm.value).subscribe(
+          (response) => {
+            if(response.status === 200) {
+              Swal.fire({
+                icon: 'success',
+                title: 'Proveedor',
+                text: 'Proveedor editado correctamente'
+              });
+              this.router.navigate(['/suppliers']);
+            }
+            else {
+              Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Ha ocurrido un error al crear el proveedor' + response.error
+              });
+            }
+          },
+          (response) => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Ha ocurrido un error al crear el proveedor' + response.error.error
+            });
+          }
+        );
+      }
+      else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, tiene errores en su formulario'
+        });
+      }
+    });
+  }
+  constructor(
+    private fb: FormBuilder,
+    private supplierService: SupplierService,
+    private router: Router,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.supplierForm = this.fb.group({
+      name: ['', Validators.required],
+      contact_name: ['', Validators.required],
+      contact_email: ['', Validators.required],
+      contact_phone: ['', Validators.required],
+      address: ['', Validators.required]
+    });
+    const navigation = this.router.getCurrentNavigation();
+    this.supplier = navigation?.extras?.state?.['supplier'];
+    this.supplierId = this.supplier?.id || 0; // Inicializar userId en el constructor
+  }
+}
+
+
---
+++ front-end/src/app/pages/products/product.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, NavigationExtras, RouterModule} from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule]
+})
+export class ProductComponent implements OnInit {
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  searchText: string = '';
+  selectedStatus: string = 'Activo'; 
+
+  constructor(private productService: ProductService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    const estado = this.selectedStatus === 'todos' ? '' : this.selectedStatus;
+    this.productService.getProduct(estado).subscribe(
+      (data) => {
+        this.products = data.data;
+        this.filteredProducts = this.products;
+        this.filterProducts();
+        console.log('Products:', this.products);
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
+  filterProducts(): void {
+    this.filteredProducts = this.products.filter(product => {
+      const name = product.name ? product.name.toLowerCase() : '';
+      const description = product.description ? product.description.toLowerCase() : '';
+      const price = product.price ? product.price.toLowerCase() : '';
+      const quantity = product.quantity ? product.quantity.toLowerCase() : '';
+      const sku = product.sku ? product.sku.toLowerCase() : '';
+      const type = product.type ? product.type.toLowerCase() : '';
+      const supplier_id = product.supplier_id ? product.supplier_id.toLowerCase() : '';
+      
+      const searchText = this.searchText.toLowerCase();
+      
+      return name.includes(searchText) ||
+        description.includes(searchText) ||
+        price.includes(searchText) ||
+        quantity.includes(searchText) ||
+        sku.includes(searchText) ||
+        type.includes(searchText) ||
+        supplier_id.includes(searchText);
+    });
+  }
+  
+
+  onSearchTextChange(): void {
+    this.filterProducts();
+  }
+
+  onStatusChange(): void {
+    this.fetchProducts();
+  }
+
+  editProduct(product: any): void {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        product
+      }
+    };
+    this.router.navigate(['/products/edit'], navigationExtras);
+  }
+
+  changeState(id: number): void {
+    this.productService.changeState(id).subscribe(
+      (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Estado',
+          text: 'Estado actualizado correctamente'})
+        this.fetchProducts();
+      },
+      (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Error al actualizar el estado'+ error})   
+      }
+    );
+  }
+}
