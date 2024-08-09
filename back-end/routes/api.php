e143ddf8d7ee37675ff86008dd3c487d35d94749
mendel-l <matt.ekisde@gmail.com>
2024-08-09 00:18:57 -0600

    edit users

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -15,6 +15,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/register', 'App\Http\Controllers\usersController@register')->name('register');
         Route::post('/all', 'App\Http\Controllers\usersController@get_all')->name('usuarios.all');
         Route::get('/change/{id}', 'App\Http\Controllers\usersController@change')->name('usuarios.change');
+        Route::post('/update/{id}', 'App\Http\Controllers\usersController@update')->name('usuarios.update');
     });
     
     Route::prefix('roles')->group(function (){
